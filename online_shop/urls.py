108b0e9fc60732381f5434d1634c92623af02dfd
nileshp-07 <patidarnilesh7223@gmail.com>
2024-11-05 22:07:59 +0530

    Add Shopping Cart functionality

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls", namespace="shop")),
+    path("cart/", include("cart.urls", namespace="cart")),
 ]
 
 
