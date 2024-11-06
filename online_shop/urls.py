3361a234ff8ec62236d4f024dc8f659e18d1eb8b
nileshp-07 <patidarnilesh7223@gmail.com>
2024-11-06 11:36:09 +0530

    Create Order functionality

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls", namespace="shop")),
     path("cart/", include("cart.urls", namespace="cart")),
+    path("orders/", include("orders.urls", namespace="orders")),
 ]
 
 
